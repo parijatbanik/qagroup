c90111c8d6fa79535ce9ddbcd6c400eaf555b7bc
Mouli Sarkar <Mouli Sarkar@192.168.0.9>
2021-04-05 13:51:33 +0530

    My first checkin

---
+++ src/main/java/LearnMaven/qagroup/RESTAPITest.java
@@ -0,0 +1,17 @@
+package LearnMaven.qagroup;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	 public void postJIRA()
+	 {
+		System.out.println("postJIRA");
+	 }
+	@Test
+	 public void deleteTwitter()
+	 {
+		System.out.println("deleteTwitter");
+	 }
+}
---
+++ src/main/java/LearnMaven/qagroup/SelenumTest.java
@@ -0,0 +1,18 @@
+package LearnMaven.qagroup;
+
+import org.testng.annotations.Test;
+
+public class SelenumTest {
+
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void ElementsUI()
+	{
+		System.out.println("ElementsUI");
+	}
+}
